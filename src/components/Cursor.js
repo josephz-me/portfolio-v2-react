554c80a5b5b58fbf6acfad13cf786b5dd0752d81
Joseph Zhang <jxsephz@gmail.com>
2020-06-14 14:09:50 -0700

    mouse removed

--- src/components/Cursor.js
+++ src/components/Cursor.js
@@ -9,14 +9,14 @@ export default function Mouse(props) {
         .cursor {
           mix-blend-mode: difference;
           pointer-events: none;
-          z-index: 1100;
+          z-index: 100;
           position: fixed;
           top: ${props.y};
           left: ${props.x};
-          width: 30px;
-          height: 30px;
-          border-radius: 50px;
-          background-color: white;
+          width: 40px;
+          height: 40px;
+          border-radius: 500px;
+          background-color: rgba(255, 255, 255, 100);
           transform: translate(-50%, -50%);
         }
       `}</style>
--- src/components/Page.js
+++ src/components/Page.js
@@ -9,16 +9,22 @@ export default function Page(props) {
   const [cursor, setCursor] = useState({ x: 0, y: 0 });
   const [ball, setBall] = useState({ x: 0, y: 0 });
 
-  const speed = 0.22;
+  const speed = 0.12;
 
   const handleMouseMove = (event) => {
+    clearTimeout(ballToCursor());
     const { clientX: mouseX, clientY: mouseY } = event;
     const distX = mouseX - ball.x;
     const distY = mouseY - ball.y;
 
     setBall({ x: ball.x + distX * speed, y: ball.y + distY * speed });
-
     setCursor({ x: ball.x, y: ball.y });
+
+    setTimeout((ballToCursor(mouseX, mouseY), 1000));
+  };
+
+  const ballToCursor = (mouseX, mouseY) => {
+    setCursor({ x: mouseX, y: mouseY });
   };
 
   return (
@@ -26,7 +32,7 @@ export default function Page(props) {
       <Meta />
       <div className={styles.navBackdrop}></div>
       <div className={styles.wrapper}>
-        <Cursor x={cursor.x + "px"} y={cursor.y + "px"}></Cursor>
+        {/* <Cursor x={cursor.x + "px"} y={cursor.y + "px"}></Cursor> */}
         <Navbar />
         {props.children}
         <Footer />
