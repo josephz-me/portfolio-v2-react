b9fc2827c56b9b3bc56fb5cd599dd1a39422f1c0
Joseph Zhang <jxsephz@gmail.com>
2020-06-16 18:48:26 -0700

    removed react-ga

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,8 @@
 import "../../styles/_global.scss";
 import React from "react";
-import ReactGA from "react-ga";
 import Page from "../components/Page";
 
-function initializeReactGA() {
-  ReactGA.initialize("UA-123791717-1");
-  ReactGA.pageview("/homepage");
-  console.log("brain");
-}
-
 export default function App({ Component, pageProps }) {
-  initializeReactGA();
   return (
     <Page>
       <Component {...pageProps} />
