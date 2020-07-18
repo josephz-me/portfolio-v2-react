e19d2427413494b526e8bff4572056f4e96c2cdf
Joseph Zhang <jxsephz@gmail.com>
2020-07-17 17:14:55 -0700

    added Skeleton

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,29 @@
 import React from "react";
 import Link from "next/link";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import styles from "./Card.module.scss";
 
 export default function Card(props) {
   return (
     <Link href={`${props.route}`}>
       <a className={styles.cards__card}>
-        {props.isImage ? (
-          <img src={props.image} />
-        ) : (
-          <video src={props.image} muted autoPlay loop playsInline />
-        )}
+        {props.isImage
+          ? <img src={props.image} /> || (
+              <SkeletonTheme
+                color="rgb(220,220,220,10)"
+                highlightColor="rgb(230,230,230,10)"
+              >
+                <Skeleton height={"30vw"} className="skeleton" />
+              </SkeletonTheme>
+            )
+          : <video src={props.image} muted autoPlay loop playsInline /> || (
+              <SkeletonTheme
+                color="rgb(220,220,220,10)"
+                highlightColor="rgb(230,230,230,10)"
+              >
+                <Skeleton height={"30vw"} className="skeleton" />
+              </SkeletonTheme>
+            )}
         <div className={styles.cards__info}>
           <p>{props.title}</p>
           <p>{props.description}</p>
