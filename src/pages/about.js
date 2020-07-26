c6e7aa70f5a59caa16acfedab536170c4a897ba4
Joseph Zhang <jxsephz@gmail.com>
2020-07-25 19:00:35 -0700

    about page bio tweak version 1209312390

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,10 +21,8 @@ export default function Home() {
             <br />
             <br />
             I'm a designer exploring various applications of multimodal
-            interaction. I believe in a future where our sources of joy,
-            purpose, and productivity are no longer constrained by the digital
-            screen. Currently, I'm an undergraduate at Carnegie Mellon pursuing
-            a{" "}
+            interaction. Currently, I'm an undergraduate at Carnegie Mellon
+            pursuing a{" "}
             <a
               target="blank"
               href="https://www.design.cmu.edu/content/bachelor-design"
