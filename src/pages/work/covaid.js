c7dc2b17bde87c1d721988d88b0c673719f1662c
Joseph Zhang <jxsephz@gmail.com>
2020-08-05 19:06:26 -0700

    covaid project update

--- src/pages/work/covaid.js
+++ src/pages/work/covaid.js
@@ -19,18 +19,17 @@ export default function Covaid() {
         </div>
 
         <div className={styles.right}>
-          <h5>— PAGE IN PROGRESS —</h5>
           <p>
-            In March 2020, I joined two recent college graduates on building
-            Covaid, a volunteer-requester matching platform that connects
-            mutual-aid organizations to those in need. The team now has +20
-            college students from across the nation contributing as social
-            outreachers, web/app developers, and product designers. Covaid now
-            collaborates with six organizations and +2000 volunteers in cities
-            such as Pittsburgh, Baltimore, and Charlotte, and Chicago.
+            Covaid is a volunteer-requester matching platform that connects
+            mutual-aid organizations to those in need. Starting with three
+            people, the team now has +20 college students from across the nation
+            contributing as social outreachers, web/app developers, and product
+            designers. Covaid now collaborates with six organizations and +2000
+            volunteers in cities such as Pittsburgh, Baltimore, and Charlotte,
+            and Chicago.
             <br />
             <br />
-            As the first designer on the team, I built the design system and
+            As the first designer on the team, I created the design system and
             interface from scratch. I now lead three other designers on
             designing various components of the application as well as
             personally contribute on the developer-side of the project.
@@ -88,7 +87,17 @@ export default function Covaid() {
             inhumane spreadsheets to manually keep track of volunteers and their
             current tasks.
           </p>
-          <Spacer />{" "}
+          <Spacer /> <p>We built three primary interfaces for Covaid:</p>
+          <br />
+          <br />
+          <em>
+            (1) Organization dashboard
+            <br />
+            (2) Volunteers dashboard
+            <br />
+            (3) Request flow
+          </em>
+          <Spacer />
         </div>
         <small className={styles.left}>
           Blurred for confidentiality purposes
@@ -99,35 +108,147 @@ export default function Covaid() {
             style={{ filter: "blur(1px)" }}
           ></img>
         </div>
-
         <Divider />
-        <h4 className={styles.left}>Interface Snapshots —</h4>
+
+        <h4 className={styles.left}>Creating a Request —</h4>
+
         <div className={styles.right}>
-          <p>We built three primary interfaces for Covaid:</p>
-          <br />
-          <br />
-          <em>
-            (1) Organization dashboard
-            <br />
-            (2) Volunteers dashboard
-            <br />
-            (3) Request flow
-          </em>
+          <p>
+            To get help, an individual simply needs to fill out a 3 step form.
+            Currently, aid is only provided in certain cities.
+          </p>
+          <Spacer />
+          <video
+            src="/static/work/covaid/create-request.mp4"
+            muted
+            autoPlay
+            loop
+            playsInline
+          ></video>
         </div>
+        <Divider />
+        <h4 className={styles.left}>Organization Dashboard —</h4>
+        <div className={styles.right}>
+          <p>
+            Organizations assign volunteers to requests by sending notifications
+            to multiple volunteers via text or email. The first to respond takes
+            on the task.
+          </p>
+          <Spacer />
+          <video
+            src="/static/work/covaid/org-match-request.mp4"
+            muted
+            autoPlay
+            loop
+            playsInline
+          ></video>
+        </div>
+        <div className={styles.right}>
+          <p>
+            To alleviate the transitional friction organizations may face,
+            Covaid syncs with Excel, the platform most organizations are coming
+            from.{" "}
+          </p>
+          <Spacer />
+          <video
+            src="/static/work/covaid/org-excel-sync.mp4"
+            muted
+            autoPlay
+            loop
+            playsInline
+          ></video>
+        </div>
+
+        <div className={styles.right}>
+          <Spacer />
+          <p>
+            Organizations are also able to view all its volunteers and their
+            contact information.
+          </p>
+          <Spacer />
+          <video
+            src="/static/work/covaid/org-view-volunteers.mp4"
+            muted
+            autoPlay
+            loop
+            playsInline
+          ></video>
+        </div>
+        <div className={styles.right}>
+          <Spacer />
+          <p>
+            Organizations are able to send mass notifications for any desired
+            purpose. We call these Beacons.
+          </p>
+          <Spacer />
+          <video
+            src="/static/work/covaid/org-create-beacon.mp4"
+            muted
+            autoPlay
+            loop
+            playsInline
+          ></video>
+        </div>
+        <Divider />
+        <h4 className={styles.left}>Volunteer Dashboard —</h4>
+        <div className={styles.right}>
+          <p>
+            Volunteers have a notification panel which displays any Beacon
+            messages sent by their organization.
+          </p>
+          <Spacer />
+          <video
+            src="/static/work/covaid/volunteer-view-beacon.mp4"
+            muted
+            autoPlay
+            loop
+            playsInline
+          ></video>
+        </div>
+        <small className={styles.left}>Handling Requests (volunteer)</small>
+        <div className={styles.right}>
+          <Spacer />
+          <p>
+            There are three states of requests: new, in-progress, and complete.
+            This is where volunteers accept or complete requests.
+          </p>
+          <Spacer />
+          <video
+            src="/static/work/covaid/volunteer-requests.mp4"
+            muted
+            autoPlay
+            loop
+            playsInline
+          ></video>
+        </div>
+        <div className={styles.right}>
+          <Spacer />
+          <p>
+            If a volunteer is unavailable, he or she is able to toggle to update
+            that. Marking oneself as inactive would prevent organization admins
+            from matching you to new requests.
+          </p>
+          <Spacer />
+          <video
+            src="/static/work/covaid/volunteer-change-active.mp4"
+            muted
+            autoPlay
+            loop
+            playsInline
+          ></video>
+        </div>
+        <Divider />
+        <h4 className={styles.left}>Mobile App</h4>
+        <p className={styles.right}>
+          Currently, the Covaid team is building a mobile app for volunteers.
+          Currently, notification via email is ineffective, annoying, and often
+          ignored.
+        </p>
         <Spacer />
-        <img src="/static/work/covaid/images/images.001.jpeg"></img>
-        <img src="/static/work/covaid/images/images.002.jpeg"></img>
-        <img src="/static/work/covaid/images/images.003.jpeg"></img>
-        <img src="/static/work/covaid/images/images.004.jpeg"></img>
-        <img src="/static/work/covaid/images/images.005.jpeg"></img>
-        <img src="/static/work/covaid/images/images.006.jpeg"></img>
-        <img src="/static/work/covaid/images/images.007.jpeg"></img>
-        <img src="/static/work/covaid/images/images.008.jpeg"></img>
         <img src="/static/work/covaid/images/images.009.jpeg"></img>
         <img src="/static/work/covaid/images/images.010.jpeg"></img>
         <img src="/static/work/covaid/images/images.011.jpeg"></img>
         <img src="/static/work/covaid/images/images.012.jpeg"></img>
-        <img src="/static/work/covaid/images/images.013.jpeg"></img>
       </div>
     </main>
   );
